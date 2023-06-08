0523f5f587f910946a500c7ccfde4340c4700a66
Mkrtich Kazanchian <mkogeo@gmail.com>
2023-06-08 17:28:58 +0400

    solved at 08.06.2023

--- DartsGame/Darts.cs
+++ DartsGame/Darts.cs
@@ -4,15 +4,28 @@ namespace DartsGame
 {
     public static class Darts
     {
-        /// <summary>
-        /// Calculates the earned points in a single toss of a Darts game.
-        /// </summary>
-        /// <param name="x">x-coordinate of dart.</param>
-        /// <param name="y">y-coordinate of dart.</param>
-        /// <returns>The earned points.</returns>
         public static int GetScore(double x, double y)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            double distance = Math.Sqrt((x * x) + (y * y));
+
+            if (distance > 10)
+            {
+                return 0;
+            }
+            else if (distance <= 10 && distance > 5)
+            {
+                return 1;
+            }
+            else if (distance <= 5 && distance > 1)
+            {
+                return 5;
+            }
+            else if (distance <= 1)
+            {
+                return 10;
+            }
+
+            return 0;
         }
     }
 }
